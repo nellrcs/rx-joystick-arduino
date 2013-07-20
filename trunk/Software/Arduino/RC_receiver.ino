b95219dd6224a92894770565fdd713b1352e3e70
elias.vds@gmail.com <elias.vds@gmail.com@08ed5484-1fb8-97ad-b854-d5559a475f6a>
2013-07-20 07:40:02 +0000

    numChannels autodetection, Arduino side.

--- trunk/Software/Arduino/RC_receiver.ino
+++ trunk/Software/Arduino/RC_receiver.ino
@@ -2,6 +2,9 @@ boolean useCompositePPM;
 boolean doubleSweep;
 int numChannels;
 
+
+// Setup //
+
 int countTo;
 unsigned long minMicros;
 unsigned long minMicrosCorr;
@@ -16,15 +19,22 @@ void setup() {
   while (true)
     if (Serial.available() > 0) {
       if ((incomingByte = Serial.read()) == '\n') {
+        useCompositePPM = lastByte & 1;
+        doubleSweep = lastByte & 2;
+        numChannels = (lastByte >> 2);
+        if (numChannels == 0)
+          numChannels = numChannels_autodetect();
+        
+        lastByte = (numChannels << 2) + (doubleSweep << 1) + useCompositePPM;
         Serial.println(lastByte);
         break;
       }
       lastByte = incomingByte;
     }
   
-   useCompositePPM = lastByte & 1;
-   doubleSweep = lastByte & 2;
-   numChannels = (lastByte >> 2) + 1;
+   //useCompositePPM = lastByte & 1;
+   //doubleSweep = lastByte & 2;
+   //numChannels = (lastByte >> 2);
    
    if (useCompositePPM)
      useCompositePPM = false; // useCompositePPM = true: Not implemented yet
@@ -45,16 +55,84 @@ void setup() {
    maxMicrosCorr = maxMicros - shCorrection;
    if (numChannels > 8)
      numChannels = 8;
+   else if (numChannels == 0)
+     numChannels = 1;
    countTo = 2 + numChannels;
   
   int pinId;
-  for (pinId = 3; pinId < countTo; pinId++)
+  for (pinId = 2; pinId < 2 + 8; pinId++) // For autodetection and to be safe if the user enters a too low numChannels value
     pinMode(pinId, INPUT);
   
   while (digitalRead(2) == HIGH); // Begin capturing after falling edge of ch0
 }
 
+
+// Autodetection at setup (non-PPM) //
+
+int numIterations = 6;
+int votes[9]; // prediction of numChannels results, max 8 channels including zero channel amount
 unsigned long times[10]; // 2 + 8: 2 is first pin on channel 0, max 8 channels with PPM with maxpulse of 2ms: 4ms sync time
+
+int numChannels_autodetect() {
+  int chId;
+  for (chId = 0; chId < 8; chId++) { // init
+    votes[chId] = 0;
+    times[2+chId] = 0;
+  }
+  
+  int i;
+  for (i = 0; i < numIterations; i++) {
+    while (digitalRead(2) == HIGH); // ensure same starting conditions
+    int prediction;
+    prediction = numChannels_autodetect_iteration();
+    if ((0 < prediction) && (prediction <= 8))
+      votes[prediction]++;
+  }
+  
+  int numChs;
+  for (numChs = 0; numChs <= 8; numChs++)
+    if (votes[numChs] > numIterations/2)
+      return numChs;
+  
+  return 0;
+}
+
+int numChannels_autodetect_iteration() {
+  int maxPrediction = 8;
+  
+  // Capture channels
+  int pinId;
+  for (pinId = 2; pinId < 10; pinId++) {
+    unsigned long startTime, waitTime;
+    waitTime = micros();
+    while (digitalRead(pinId) == LOW) {
+      if (micros() - waitTime > 22000) { // 22ms, exceeding total PPM period, so can't be a valid channel
+        maxPrediction = pinId - 2;
+        goto verifyMicros;
+      }
+    }
+    startTime = micros();
+    while (digitalRead(pinId) == HIGH) {
+      if (micros() - startTime > 2524) { // exceeding maxMicros, even in case of doubleSweep: 2524us, or exceeding total PPM period, so can't be a valid channel
+        maxPrediction = pinId - 2;
+        goto verifyMicros;
+      }
+    }
+    times[pinId] = micros() - startTime;
+  }
+  
+  // Verify all channels past the test for now
+  verifyMicros:
+    int chId;
+    for (chId = 0; chId < maxPrediction; chId++)
+      if (times[2+chId] < 476) // lower than minMicros, even in case of doubleSweep: 476us
+        return chId;
+  
+  return maxPrediction;
+}
+
+
+// Processing (non-PPM) //
 
 void loop() {
   // Capture channels
