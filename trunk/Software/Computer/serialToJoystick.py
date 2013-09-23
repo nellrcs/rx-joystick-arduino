693b4644012e1813c527fcfb9d3a5bcf142222e2
elias.vds@gmail.com <elias.vds@gmail.com@08ed5484-1fb8-97ad-b854-d5559a475f6a>
2013-09-23 10:27:15 +0000

    - Use an enhanced fuzz-flat filter. - Support PPM mode. - Add mode-autodetection. - Add period-autodetection. - Fix typo in Flightgear configuration file.

--- trunk/Software/Computer/serialToJoystick.py
+++ trunk/Software/Computer/serialToJoystick.py
@@ -1,9 +1,8 @@
 # -*- coding: utf-8 -*-
 import serial
 import os
-import time, math
+import time, math, struct
 
-#debugData = "" # TODO: remove (debug)
 packetCounter = 0
 
 
@@ -11,15 +10,14 @@ packetCounter = 0
 controllerName = None
 numChannels = 0
 arduinoDevice = None
+useCompositePPM = -1
 doubleSweep = False
-useCompositePPM = False
 useDummyJoystick = False
+filteringFuzz = 6
 
 maxLatency = 100e-3 # 100ms
-doFiltering = True
-filteringTolerance = 8
-
-PPM_Period = 22e-3 # 22ms
+periodFound = False # Measured period found
+PPM_Period = 20e-3 # Default period: 20ms
 
 
 
@@ -62,47 +60,68 @@ def connectToArduino():
 
 
 def connectToRCreceiver(ser):
-	global numChannels
-	command = (numChannels << 2) + (doubleSweep << 1) + useCompositePPM
+	global numChannels, useCompositePPM, PPM_Period, periodFound
+	command = (numChannels << 3) + (useCompositePPM << 2) + (modeAutodetect << 1) + doubleSweep
 	
-	numRetries = 4
+	numRetries = 5
 	for i in range(numRetries):
-		ser.write(chr(command) + "\n")
+		# Send command
+		ser.write(chr(command) + "\xff")
+		
+		# Check response format
 		response = ser.readline()
-		valid = (len(response) == 1 + 2 and response[1:3] == "\r\n")
+		valid = (len(response) == 1 + 4 + 2 and response[-2:] == "\r\n")
 		if not valid:
 			continue
-		response = ord(response[0])
+		command_received = ord(response[0])
+		numChannels_received = command_received >> 3
+		useCompositePPM_received = (command_received & (1 << 2)) >> 2
+		modeAutodetect_received = (command_received & (1 << 1)) >> 1
+		doubleSweep_received = command_received & 1
+		valid = (doubleSweep_received == doubleSweep)
+		if not modeAutodetect:
+			valid = (valid and useCompositePPM_received == useCompositePPM and modeAutodetect_received == modeAutodetect)
 		if numChannels:
-			# Manual channel amount
-			valid = (response == command)
-			if not valid:
-				continue
-		else:
-			# Automatic channel amount
-			numChannels_received = response >> 2
-			doubleSweep_received = response & 2
-			useCompositePPM_received = response & 1
-			valid = ((doubleSweep_received, useCompositePPM_received) == (doubleSweep, useCompositePPM))
-			if not valid:
-				continue
+			valid = (valid and numChannels_received == numChannels)
+		if not valid:
+			continue
+		
+		# Check respons results
+		if modeAutodetect: # Automatic mode
+			valid = not modeAutodetect_received
+			if valid:
+				useCompositePPM = useCompositePPM_received
+				print ("Autodetected %s mode." % ("separate PWM", "composite PPM")[useCompositePPM])
+			else:
+				print ("Autodetection of mode failed, \nmake sure you're RC transmitter (and receiver if using channel outputs,) is powered on.")
+		if not numChannels: # Automatic channel amount
 			valid = (1 <= numChannels_received <= 8)
 			if valid:
 				numChannels = numChannels_received
 				print ("Autodetected %s channel(s)." % numChannels)
 			else:
 				if not numChannels_received:
-					print ("Autodetection of amount of channels failed, \nmake sure you're RC transmitter is powered on.")
+					print ("Autodetection of amount of channels failed, \nmake sure you're RC transmitter%s is powered on." % (" (and receiver)", "")[useCompositePPM])
 				else:
 					print ("Autodetection of amount of channels is assumed to be faulty, since it's greater that 8.")
+		else: # Manual channel amount
+			pass
+		
+		# Store period
+		period_received = struct.unpack('<L', response[1:1+4])[0]
+		if period_received:
+			periodFound = True
+			PPM_Period = period_received * 1e-6
+			print ("Detected refresh rate of %.0f Hz." % round(1./PPM_Period))
+		
 		break
 	
 	if not valid:
-		print ("Failed to connect to RC receiver via Arduino on '%s'." % arduinoDevice)
+		print ("Failed to connect to RC transmitter/receiver via Arduino on '%s'." % arduinoDevice)
 		return False
 	
-	print ("Connected to RC receiver. (%s channels via %s)" % \
-			(numChannels, ("channel connectors", "battery connector")[useCompositePPM]))
+	print ("Connected to RC %s. (%s channel(s) via %s)" % \
+			(("receiver", "transmitter")[useCompositePPM], numChannels, ("channel connectors", "D.S.C. connector")[useCompositePPM]))
 	
 	return True
 
@@ -120,7 +139,6 @@ class UInputJoystick(Joystick): # Works on Linux
 		import uinput
 		events = [(3, axisId) + (0, 255, 0, 0) for axisId in range(self.numAxis)]
 		self.joy = uinput.Device(events, name = self.name, bustype = 0x03) # bustype = BUS_USB
-		#writeJoystick(self, chr(128) * numAxis) # reset joystick controls
 	def emit(self, axisId, value, syn = True):
 		self.joy.emit((3, axisId), value, syn) # uinput.ABS_Ch(axisId = (3, axisId)
 
@@ -144,7 +162,7 @@ class DummyJoystick(Joystick): # Platform independent, but no externally usable 
 def createJoystick():
 	global controllerName
 	if not controllerName:
-		controllerName = "RC Receiver (%schs)" % numChannels
+		controllerName = "RC Controller (%s mode) (%schs)" % (("PWM", "PPM")[useCompositePPM], numChannels)
 	
 	def createUInputJoystick():
 		joy = UInputJoystick(numChannels, name = controllerName)
@@ -168,7 +186,6 @@ def createJoystick():
 
 
 def readRCreceiver(ser, joy):
-	#global debugData # TODO: remove (debug)
 	global packetCounter
 	buffer = ""
 
@@ -181,48 +198,47 @@ def readRCreceiver(ser, joy):
 			buffer = ""
 			print ("Buffer overrun occured. Resetting buffer.")
 		
-		#time.sleep(20e-3 / 2) # 20ms / 2
 		if ser.inWaiting():
 			newData = ser.read(ser.inWaiting())
 		else:
 			newData = ser.read() # block cpu
 		buffer += newData
-		#debugData += newData # TODO: remove (debug)
 		if '\xff' in buffer:
 			data, buffer = buffer.split('\xff')[-2:]
 			if len(data) == numChannels:
 				writeJoystick(joy, processData(data))
 				packetCounter += 1
-			#print (data) # TODO: remove (debug)
 
+
+class Filter:
+	def __init__(self, fuzz):
+		self.fuzz = fuzz
+		self.left = self.right = None
+	def process(self, data):
+		if self.left == None: # and self.right == None:
+			self.left = self.right = data
+		elif max(data - self.left, self.right - data) >= self.fuzz:
+			self.left = self.right = data
+		elif data < self.left:
+			self.left = data
+			return data # Return unfiltered value to ensure quick response
+		elif self.right < data:
+			self.right = data
+			return data # Return unfiltered value to ensure quick response
+		return (self.left + self.right) / 2
 
 def initFilter():
-	global lastlastData, lastData, holdLastData
-	lastlastData = numChannels * [127]
-	lastData     = numChannels * [127]
-	holdLastData = numChannels * [False]
+	global filters
+	filters = [Filter(filteringFuzz) for i in range(numChannels)]
 
 
 def processData(data):
-	global lastlastData, lastData, holdLastData
 	data = list(data)
-	#unf = list(data)
 	for i, d in enumerate(data):
 		data[i] = ord(d) - 1
 		if data[i] >= 253:
 			data[i] = 255
-		#unf[i] = data[i]
-		# Filtering of arduinos timing granularity at a frequency of about 45Hz
-		if doFiltering:
-			if holdLastData[i]:
-				holdLastData[i] = (data[i] == lastData[i])
-				if abs(data[i] - lastData[i]) < filteringTolerance:
-					data[i] = lastData[i]
-			elif data[i] == lastlastData[i]:
-				holdLastData[i] = True
-	lastlastData = lastData
-	lastData = data
-	#print (data, unf)
+		data[i] = filters[i].process(data[i])
 	return data
 
 
@@ -240,12 +256,10 @@ def main():
 		if ser:
 			ser.close()
 		
-		#print (debugData.split("\xff")) # TODO: remove this
-		#packetCounter = len(debugData.split("\xff")) # TODO: remove this
 		if joy:
-			#print ("Time of CPU:", endTime - startTime) # TODO: remove this
 			expectedNbPackets = (endTime-startTime) / PPM_Period
-			print ("%s packets captured (%s%%)" % (packetCounter, int(100 * packetCounter/expectedNbPackets)))
+			if periodFound:
+				print ("%s packets captured (%.0f%%)" % (packetCounter, round(100. * packetCounter/expectedNbPackets)))
 		
 		print ("Connections closed.")
 
@@ -259,6 +273,7 @@ def main():
 		joy = createJoystick()
 		if joy:
 			initFilter()
+			writeJoystick(joy, [127] * numChannels) # reset joystick controls
 			startTime = time.time()
 			try:
 				readRCreceiver(ser, joy)
@@ -275,23 +290,27 @@ if __name__ == "__main__":
 	parser = argparse.ArgumentParser(description = 'Creates a virtual joystick controller using data from a RC receiver via an Arduino. '+ 
 		'Go to http://code.google.com/p/rx-joystick-arduino/ for more info.')
 		
+	parser.add_argument('-m, --mode', dest = 'useCompositePPM', action='store',
+		default = useCompositePPM, type = int, choices = [-1, 0, 1],
+		help = "the type of source we'll be capturing from: '0': use multiple separate PWM channel inputs (from receiver outputs); '1': use one combined PPM input (e.g. from transmitter's D.S.C. output) as input to capture all channels; if not provided or '-1' is used, autodetection will be performed, PWM mode has the highest priority")
 	parser.add_argument('-c, --num-channels', dest = 'numChannels', action = 'store',
-		default = numChannels, type = int, choices = xrange(0, 1+8),
+		default = numChannels, type = int, choices = range(0, 1+8),
 		help = "the exact amount of channels used, if not provided or '0' is used, autodetection will be performed")
+	parser.add_argument('-f, --filtering-fuzz', dest = 'filteringFuzz', action = 'store',
+		default = filteringFuzz, type = int, choices = range(1, 1+16),
+		help = "the number of adjacent values that are result of quantization noise, if '1' is used, no filtering will be performed; default value is '%s'" % filteringFuzz)
 
 	parser.add_argument('-n, --name', dest = 'controllerName', action = 'store',
-			default = controllerName,
-			help = 'the name of the virtual joystick, how it will appear to other applications, if not provided, autogeneration will occur')
+		default = controllerName,
+		help = 'the name of the virtual joystick, how it will appear to other applications, if not provided, autogeneration will occur')
 	parser.add_argument('-d, --device', dest = 'arduinoDevice', action = 'store',
-			default = arduinoDevice,
-			help = "the device path of the Arduino, on Linux usually something like '/dev/ttyACM0', if not provided, autodetection will be performed")
+		default = arduinoDevice,
+		help = "the device path of the Arduino, on Linux usually something like '/dev/ttyACM0', if not provided, autodetection will be performed")
 
-	parser.add_argument('-p, --use-ppm', dest = 'useCompositePPM', action='store_true',
-			help = 'use one PPM stream as input to capture all channels, instead of using multiple channel streams')
 	parser.add_argument('-D, --double-sweep', dest = 'doubleSweep', action = 'store_true',
-			help = 'double the range of all channels, resolution will be halved')
+		help = "double the range of all channels, resolution will be halved")
 	parser.add_argument('-t, --test', dest = 'useDummyJoystick', action = 'store_true',
-			help = "print and visualise every controller event update, don't create an externally accessible virtual joystick")
+		help = "print and visualise every controller event update, don't create an externally accessible virtual joystick")
 
 	args = parser.parse_args()
 
@@ -299,13 +318,11 @@ if __name__ == "__main__":
 	controllerName = args.controllerName
 	arduinoDevice = args.arduinoDevice
 	useCompositePPM = args.useCompositePPM
+	modeAutodetect = (useCompositePPM == -1)
+	useCompositePPM = max(0, useCompositePPM)
 	doubleSweep = args.doubleSweep
 	useDummyJoystick = args.useDummyJoystick
+	filteringFuzz = args.filteringFuzz
 	
 	
-
-	if useCompositePPM:
-		print ("PPM input is not yet implemented. Using channels instead.")
-		useCompositePPM = False
-
 	main()
